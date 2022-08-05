deb0c9500bed7a1475365ea7d82251e547228e89
Elhadj abdoulaye Diallo <elhadjabdoulaye.diallo.etu@univ-lille.fr>
2022-08-05 02:11:58 +0200

    updates

--- src/Kernel.php
+++ src/Kernel.php
@@ -17,11 +17,22 @@ class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
+<<<<<<< ours
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
+=======
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+
+        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+            $container->import('../config/services.yaml');
+            $container->import('../config/{services}_'.$this->environment.'.yaml');
+        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+            (require $path)($container->withPath($path), $this);
+>>>>>>> theirs
         }
     }
 
@@ -30,6 +41,7 @@ class Kernel extends BaseKernel
         return \dirname(__DIR__);
     }
 
+<<<<<<< ours
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
@@ -50,5 +62,12 @@ class Kernel extends BaseKernel
         $routes->import($confDir.'/{routes}/'.$this->environment.'/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+=======
+        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+            $routes->import('../config/routes.yaml');
+        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+            (require $path)($routes->withPath($path), $this);
+        }
+>>>>>>> theirs
     }
 }
